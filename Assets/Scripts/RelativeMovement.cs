299c60b4d4539091f5a0ee4e56fe0135a763e4b5
Hao He <hehao1998@pku.edu.cn>
2018-04-05 12:45:11 +0800

    Prototype Version 1

---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour {
+
+	[SerializeField] private Transform target;
+
+	public float rotateSpeed = 15.0f;
+	public float moveSpeed = 6.0f;
+	public float jumpSpeed = 15.0f;
+	public float gravity = -9.8f;
+	public float terminalVelocity = -10.0f;
+	public float minFall = -1.5f;
+
+	private float vertSpeed;
+
+	private CharacterController charController;
+	private ControllerColliderHit contact;
+
+	private Animator animator;
+
+	void Start() {
+		charController = GetComponent<CharacterController> ();
+		animator = GetComponent<Animator> ();
+		vertSpeed = minFall;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = Vector3.zero;
+
+		float horInput = Input.GetAxis ("Horizontal");
+		float vertInput = Input.GetAxis ("Vertical");
+		if (horInput != 0 || vertInput != 0) {
+			movement.x = horInput * moveSpeed;
+			movement.z = vertInput * moveSpeed;
+			movement = Vector3.ClampMagnitude (movement, moveSpeed);
+
+			Quaternion tmp = target.rotation;
+			target.eulerAngles = new Vector3 (0, target.eulerAngles.y, 0);
+			movement = target.TransformDirection (movement);
+			target.rotation = tmp;
+
+			// Linear Interpolate rotation movement to make it smoother
+			Quaternion direction = Quaternion.LookRotation(movement);
+			transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotateSpeed * Time.deltaTime);
+		}
+
+		animator.SetFloat ("Speed", movement.sqrMagnitude);
+
+		// Check jump
+		bool hitGround = false;
+		RaycastHit hit;
+		if (vertSpeed < 0 && Physics.Raycast (transform.position, Vector3.down, out hit)) {
+			float check = (charController.height + charController.radius) / 1.9f;
+			hitGround = hit.distance <= check;
+		}
+		if (hitGround) {
+			if (Input.GetButton ("Jump")) {
+				vertSpeed = jumpSpeed;
+			} else {
+				vertSpeed = minFall;
+				animator.SetBool ("Jumping", false);
+			}
+		} else {
+			vertSpeed += gravity * 5 * Time.deltaTime;
+			if (vertSpeed < terminalVelocity) {
+				vertSpeed = terminalVelocity;
+			}
+
+			if (contact != null) {
+				animator.SetBool ("Jumping", true);
+			}
+
+			if (charController.isGrounded) {
+				if (Vector3.Dot (movement, contact.normal) < 0) {
+					movement = contact.normal * moveSpeed;
+				} else {
+					movement += contact.normal * moveSpeed;
+				}
+			}
+		}
+		movement.y = vertSpeed;			
+
+		movement *= Time.deltaTime;
+		charController.Move (movement);
+	}
+
+	void OnControllerColliderHit( ControllerColliderHit hit) {
+		contact = hit;
+	}
+}
---
+++ Assets/Scripts/WonderingAI.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// some code adjusted from https://www.jianshu.com/p/43148b88765f
+public class WonderingAI : MonoBehaviour {
+	public float speed = 3.0f;
+	public float obstacleRange = 2.5f;
+
+	public float HorSight = 120f;
+	public float VerSight = 60f;
+	public float sightDis = 10f;
+
+	private NavMeshAgent agent;
+
+	public Transform[] points;
+	private int destPoint = 0;
+
+	[SerializeField] private Transform target;
+	private Vector3 leftDir, rightDir, upDir, downDir;
+
+	private enum State {
+		Wondering, Seeking, Alerted, Stopped
+	};
+	private State state;
+
+	void Start() {
+		state = State.Wondering;
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update() {
+		bool inSight = InSight ();
+		if (state == State.Wondering && inSight) {
+			// Debug.Log ("player discovered!");
+			state = State.Seeking;
+		} else if (!inSight && state == State.Seeking){
+			Debug.Log ("Setting state to wondering");
+			state = State.Wondering;
+		}
+
+		// AI behaves according to its current state
+		switch (state) {
+		case State.Wondering:
+			//Debug.Log (lastKnownPos);
+			if (!agent.pathPending && agent.remainingDistance < 0.5f)
+				GotoNextPoint();
+			break;
+		case State.Seeking:
+			agent.destination = target.position;
+			break;
+		case State.Alerted:
+			
+			break;
+		case State.Stopped:
+			
+			break;
+		default:
+			break;
+		}
+
+	}
+		
+
+	void GotoNextPoint() {
+		// Returns if no points have been set up
+		if (points.Length == 0)
+			return;
+
+		// Set the agent to go to the currently selected destination.
+		agent.destination = points[destPoint].position;
+
+		// Choose the next point in the array as the destination,
+		// cycling to the start if necessary.
+		destPoint = Random.Range(0, points.Length - 1);
+	}
+
+	// Determine whether the player is in sight or not
+	private bool InSight() {
+		Vector3 tempForward = transform.forward;
+
+		Quaternion left = Quaternion.AngleAxis (-HorSight / 2, transform.up);
+		Quaternion right = Quaternion.AngleAxis (HorSight / 2, transform.up);
+		Quaternion up = Quaternion.AngleAxis (-VerSight / 2, transform.right);
+		Quaternion down = Quaternion.AngleAxis (VerSight / 2, transform.right);
+
+		leftDir = left * tempForward;
+		rightDir = right * tempForward;
+		upDir = up * tempForward;
+		downDir = down * tempForward;
+
+		Debug.DrawLine (transform.position, transform.position + leftDir * sightDis, Color.blue);
+		Debug.DrawLine (transform.position, transform.position + rightDir * sightDis, Color.red);
+		Debug.DrawLine (transform.position, transform.position + upDir * sightDis, Color.white);
+		Debug.DrawLine (transform.position, transform.position + downDir * sightDis, Color.green);
+
+		// In the sight distance, then determine whether it's really in the sight
+		if (Vector3.Distance(transform.position,target.position) < sightDis) {
+			Vector3 dir = target.transform.position - transform.position;
+
+			// Judge whether there are obstacles between player and AI
+			Ray ray = new Ray(transform.position, dir);
+			RaycastHit hit;
+			if (Physics.SphereCast (ray, 1.0f, out hit)) {
+				GameObject hitObject = hit.transform.gameObject;
+				if (hitObject.name != "Player") {
+					// Debug.Log ("Obstacles between player and this AI");
+					return false;
+				}
+			}
+
+			Vector3 targetHorPos = new Vector3(dir.x,leftDir.y, dir.z);
+			Vector3 targetVerPos = new Vector3(upDir.x, dir.y, dir.z);
+
+			Vector3 leftCross = Vector3.Cross(targetHorPos,leftDir);
+			Vector3 rightCross = Vector3.Cross (targetHorPos,rightDir);
+
+			Vector3 upCross = Vector3.Cross (targetVerPos, upDir);
+			Vector3 downCross = Vector3.Cross (targetVerPos, downDir);
+
+			if (Vector3.Dot(dir, transform.forward) > 0) {
+
+				if (Vector3.Dot(leftCross, rightCross) < 0) {
+					// Debug.Log("In horizontal sight");
+					return true;
+				}
+				if (Vector3.Dot(upCross, downCross) < 0) {
+					// Debug.Log("In vertical sight");
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
